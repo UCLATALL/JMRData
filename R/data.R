e01ed665812c4da4eff6de0edc5719834573539c
Adam Blake <theadamattack@gmail.com>
2018-12-18 11:05:53 -0800

    Add descriptions, rename 7.1 to 7.2, add 13.1

--- R/data.R
+++ R/data.R
@@ -1,10 +1,12 @@
 #' Data for Example 1.1
 #'
-#' [Some description here]
+#' Internet access rates for each of the 50 U.S. states.
+#'
+#' TODO: what are the units of the rates?
 #'
 #' @format A data frame with 50 observations of 3 variables:
 #' \describe{
-#'   \item{State}{}
+#'   \item{State}{The two-letter abbreviation of the state.}
 #'   \item{Internet}{}
 #'   \item{College}{}
 #' }
@@ -12,17 +14,29 @@
 
 #' Data for Example 4.1
 #'
-#' [Some description here]
+#' Lottery bids for 20 bidders in hypothetical lottery.
 #'
 #' @format A data frame with 20 observations of 1 variable:
 #' \describe{
-#'   \item{bids}{}
+#'   \item{bids}{Bid in dollars.}
 #' }
 "ex4.1"
 
+#' Lottery dataset
+#'
+#' Lottery bids for 20 bidders in hypothetical lottery.
+#'
+#' @format A data frame with 20 observations of 1 variable:
+#' \describe{
+#'   \item{bids}{Bid in dollars.}
+#' }
+"lottery"
+
 #' Data for Example 5.1
 #'
-#' [Some description here]
+#' Internet access rates and college education rates for each of the 50 U.S. states.
+#'
+#' TODO: what are the units of the rates?
 #'
 #' @format A data frame with 50 observations of 3 variables:
 #' \describe{
@@ -34,146 +48,150 @@
 
 #' Data for Example 6.2
 #'
-#' [Some description here]
+#' Sex, age (years), height (in.), and weight (lbs.) of 19 middle school students.
 #'
 #' @format A data frame with 19 observations of 5 variables:
 #' \describe{
-#'   \item{Name}{}
-#'   \item{Sex}{}
-#'   \item{Age}{}
-#'   \item{Height}{}
-#'   \item{Weight}{}
+#'   \item{Name}{Student's name}
+#'   \item{Sex}{Sex of the student: coded as F or M}
+#'   \item{Age}{Age of the student in years}
+#'   \item{Height}{Height of the student in inches}
+#'   \item{Weight}{Weight of the student in pounds}
 #' }
 "ex6.2"
 
-#' Data for Example 7.1
+#' Data for Example 7.2
 #'
-#' [Some description here]
+#' Running time, age, and miles of training per week for hypothetical runners.
 #'
 #' @format A data frame with 80 observations of 3 variables:
 #' \describe{
-#'   \item{TIME}{}
-#'   \item{AGE}{}
-#'   \item{MILES}{}
+#'   \item{TIME}{Amount of time spent running per week}
+#'   \item{AGE}{Age of the runner}
+#'   \item{MILES}{Number of miles run per week}
 #' }
-"ex7.1"
+"ex7.2"
 
 #' Data for Example 8.1
 #'
-#' [Some description here]
+#' SAT scores for two groups of student who did or did not take a prep course.
 #'
 #' @format A data frame with 20 observations of 3 variables:
 #' \describe{
-#'   \item{Student}{}
-#'   \item{Group}{}
-#'   \item{SAT}{}
+#'   \item{Student}{Student ID}
+#'   \item{Group}{Whether the students took the \code{course} or not \code{noCourse}}
+#'   \item{SAT}{Score on the SAT}
 #' }
 "ex8.1"
 
 #' Data for Example 8.6
 #'
-#' [Some description here]
+#' Subsequent tasks completed after different types of feedback on a previous task.
 #'
 #' @format A data frame with 24 observations of 2 variables:
 #' \describe{
-#'   \item{Condition}{}
-#'   \item{Task}{}
+#'   \item{Condition}{Feeback given: failure, none, success}
+#'   \item{Task}{Score on the task}
 #' }
 "ex8.6"
 
 #' Data for Example 8.13
 #'
-#' [Some description here]
+#' GRE scores from students in one of three training programs in psychology.
 #'
 #' @format A data frame with 18 observations of 2 variables:
 #' \describe{
-#'   \item{Program}{}
-#'   \item{GRE}{}
+#'   \item{Program}{The program: clinical, developmental, or social}
+#'   \item{GRE}{Student's GRE score}
 #' }
 "ex8.13"
 
 #' Data for Example 8.15
 #'
-#' [Some description here]
+#' Scores on standardized arithmetic test for hypothetical schoolchildren.
 #'
 #' @format A data frame with 15 observations of 2 variables:
 #' \describe{
-#'   \item{Grade}{}
-#'   \item{Score}{}
+#'   \item{Grade}{Student's current grade in school: 4, 5, or 6}
+#'   \item{Score}{Student's score on the test}
 #' }
 "ex8.15"
 
 #' Data for Example 9.1
 #'
-#' [Some description here]
+#' Mood scores from a drug x psychotherapy (treatment, control) factorial design.
 #'
 #' @format A data frame with 50 observations of 3 variables:
 #' \describe{
-#'   \item{Drug}{}
-#'   \item{Psychotherapy}{}
-#'   \item{Mood}{}
+#'   \item{Drug}{The drug given: A, B, or Placebo}
+#'   \item{Psychotherapy}{Type of psychotherapy administered: treatment or control}
+#'   \item{Mood}{Patient's mood score}
 #' }
 "ex9.1"
 
 #' Data for Example 10.1
 #'
-#' [Some description here]
+#' Acheivement and pretest scores for students under different curricula and teachers (effect coded groups).
 #'
 #' @format A data frame with 40 observations of 4 variables:
 #' \describe{
-#'   \item{score}{}
-#'   \item{curriculum}{}
-#'   \item{teacher}{}
-#'   \item{pretest}{}
+#'   \item{score}{The student's achievement score}
+#'   \item{curriculum}{-1 = old, 1 = new}
+#'   \item{teacher}{-1 = A, 1 = B}
+#'   \item{pretest}{The student's score on a pretest}
 #' }
 "ex10.1"
 
 #' Data for Example 10.8
 #'
-#' [Some description here]
+#' Modified version of ex10.1.txt; see textbook for explanation.
+#'
+#' TODO: update explanation
 #'
 #' @format A data frame with 40 observations of 4 variables:
 #' \describe{
-#'   \item{score}{}
-#'   \item{curriculum}{}
-#'   \item{teacher}{}
-#'   \item{pretest}{}
+#'   \item{score}{The student's achievement score}
+#'   \item{curriculum}{-1 = old, 1 = new}
+#'   \item{teacher}{-1 = A, 1 = B}
+#'   \item{pretest}{The student's score on a pretest}
 #' }
 "ex10.8"
 
 #' Data for Example 10.12
 #'
-#' [Some description here]
+#' Psychological symptoms and number of criticisms across two conditions.
 #'
 #' @format A data frame with 6 observations of 5 variables:
 #' \describe{
-#'   \item{Condition}{}
-#'   \item{Criticism}{}
-#'   \item{Symptoms}{}
+#'   \item{Condition}{Whether the participant was in the treatment or control group}
+#'   \item{Criticism}{Number of criticisms}
+#'   \item{Symptoms}{Number of symptoms}
 #' }
 "ex10.12"
 
 #' Data for Example 11.1
 #'
-#' [Some description here]
+#' Three within-group scores for two levels of competition.
 #'
 #' @format A data frame with 6 observations of 5 variables:
 #' \describe{
-#'   \item{group}{}
-#'   \item{instructions}{}
-#'   \item{score1}{}
-#'   \item{score2}{}
-#'   \item{score3}{}
+#'   \item{group}{Group ID}
+#'   \item{instructions}{Whether the participants were instructed they were in \code{competition} or not (\code{none})}
+#'   \item{score1}{Score on first trial}
+#'   \item{score2}{Score on second trial}
+#'   \item{score3}{Score on third trial}
 #' }
 "ex11.1"
 
 #' Data for Example 11.9
 #'
-#' [Some description here]
+#' Data for crossed design (experimenter present or absent)
+#'
+#' TODO: What do the scores represent?
 #'
 #' @format A data frame with 8 observations of 3 variables:
 #' \describe{
-#'   \item{subject}{}
+#'   \item{subject}{Subject ID}
 #'   \item{absent}{}
 #'   \item{present}{}
 #' }
@@ -181,7 +199,9 @@
 
 #' Data for Example 11.17
 #'
-#' [Some description here]
+#' Data for a 3 x 2 within-subject design
+#'
+#' TODO: what was the experiment?
 #'
 #' @format A data frame with 5 observations of 7 variables:
 #' \describe{
@@ -197,24 +217,32 @@
 
 #' Data for Example 11.22
 #'
-#' [Some description here]
+#' Scores for husbands and wives by couple with years married and child status.
+#'
+#' TODO: What are the couple, Male and Female scores?
 #'
 #' @format A data frame with 16 observations of 5 variables:
 #' \describe{
 #'   \item{couple}{}
-#'   \item{children}{}
-#'   \item{yearsmarried}{}
+#'   \item{children}{Does the couple have children? ys or no}
+#'   \item{yearsmarried}{Number of years married}
 #'   \item{Male}{}
 #'   \item{Female}{}
 #' }
 "ex11.22"
 
-#' Lottery dataset
+#' Data for Example 13.1
 #'
-#' [Some description here]
+#' SAT and high school ranks with and without an outlier induced by reversing variable values for one observations.
 #'
-#' @format A data frame with 20 observations of 1 variable:
+#' TODO: these don't appear to be reversed. Check textbook for description and method
+#'
+#' @format A data frame with 13 observations of 5 variables:
 #' \describe{
-#'   \item{bids}{}
+#'   \item{student}{Student ID}
+#'   \item{sat}{Student's SAT score}
+#'   \item{hsrank}{Student's rank in high school}
+#'   \item{satr}{Reversed values of \code{sat}}
+#'   \item{hsrankr}{Reversed values of \code{hsrankr}}
 #' }
-"lottery"
+"ex13.1"
