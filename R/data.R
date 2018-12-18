ebd9a013666519aed0302412adb093a079b87d40
Adam Blake <theadamattack@gmail.com>
2018-12-17 19:15:42 -0800

    Add bare-bones documentation

---
+++ R/data.R
@@ -0,0 +1,220 @@
+#' Data for Example 1.1
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 50 observations of 3 variables:
+#' \describe{
+#'   \item{State}{}
+#'   \item{Internet}{}
+#'   \item{College}{}
+#' }
+"ex1.1"
+
+#' Data for Example 4.1
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 20 observations of 1 variable:
+#' \describe{
+#'   \item{bids}{}
+#' }
+"ex4.1"
+
+#' Data for Example 5.1
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 50 observations of 3 variables:
+#' \describe{
+#'   \item{State}{}
+#'   \item{Internet}{}
+#'   \item{College}{}
+#' }
+"ex5.1"
+
+#' Data for Example 6.2
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 19 observations of 5 variables:
+#' \describe{
+#'   \item{Name}{}
+#'   \item{Sex}{}
+#'   \item{Age}{}
+#'   \item{Height}{}
+#'   \item{Weight}{}
+#' }
+"ex6.2"
+
+#' Data for Example 7.1
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 80 observations of 3 variables:
+#' \describe{
+#'   \item{TIME}{}
+#'   \item{AGE}{}
+#'   \item{MILES}{}
+#' }
+"ex7.1"
+
+#' Data for Example 8.1
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 20 observations of 3 variables:
+#' \describe{
+#'   \item{Student}{}
+#'   \item{Group}{}
+#'   \item{SAT}{}
+#' }
+"ex8.1"
+
+#' Data for Example 8.6
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 24 observations of 2 variables:
+#' \describe{
+#'   \item{Condition}{}
+#'   \item{Task}{}
+#' }
+"ex8.6"
+
+#' Data for Example 8.13
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 18 observations of 2 variables:
+#' \describe{
+#'   \item{Program}{}
+#'   \item{GRE}{}
+#' }
+"ex8.13"
+
+#' Data for Example 8.15
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 15 observations of 2 variables:
+#' \describe{
+#'   \item{Grade}{}
+#'   \item{Score}{}
+#' }
+"ex8.15"
+
+#' Data for Example 9.1
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 50 observations of 3 variables:
+#' \describe{
+#'   \item{Drug}{}
+#'   \item{Psychotherapy}{}
+#'   \item{Mood}{}
+#' }
+"ex9.1"
+
+#' Data for Example 10.1
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 40 observations of 4 variables:
+#' \describe{
+#'   \item{score}{}
+#'   \item{curriculum}{}
+#'   \item{teacher}{}
+#'   \item{pretest}{}
+#' }
+"ex10.1"
+
+#' Data for Example 10.8
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 40 observations of 4 variables:
+#' \describe{
+#'   \item{score}{}
+#'   \item{curriculum}{}
+#'   \item{teacher}{}
+#'   \item{pretest}{}
+#' }
+"ex10.8"
+
+#' Data for Example 10.12
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 6 observations of 5 variables:
+#' \describe{
+#'   \item{Condition}{}
+#'   \item{Criticism}{}
+#'   \item{Symptoms}{}
+#' }
+"ex10.12"
+
+#' Data for Example 11.1
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 6 observations of 5 variables:
+#' \describe{
+#'   \item{group}{}
+#'   \item{instructions}{}
+#'   \item{score1}{}
+#'   \item{score2}{}
+#'   \item{score3}{}
+#' }
+"ex11.1"
+
+#' Data for Example 11.9
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 8 observations of 3 variables:
+#' \describe{
+#'   \item{subject}{}
+#'   \item{absent}{}
+#'   \item{present}{}
+#' }
+"ex11.9"
+
+#' Data for Example 11.17
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 5 observations of 7 variables:
+#' \describe{
+#'   \item{Subject}{}
+#'   \item{abstract1}{}
+#'   \item{concrete1}{}
+#'   \item{abstract2}{}
+#'   \item{concrete2}{}
+#'   \item{abstract3}{}
+#'   \item{concrete3}{}
+#' }
+"ex11.17"
+
+#' Data for Example 11.22
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 16 observations of 5 variables:
+#' \describe{
+#'   \item{couple}{}
+#'   \item{children}{}
+#'   \item{yearsmarried}{}
+#'   \item{Male}{}
+#'   \item{Female}{}
+#' }
+"ex11.22"
+
+#' Lottery dataset
+#'
+#' [Some description here]
+#'
+#' @format A data frame with 20 observations of 1 variable:
+#' \describe{
+#'   \item{bids}{}
+#' }
+"lottery"
