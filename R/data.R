0b78ebe9cd94269c8aa1bee290f9843b1faa5cd3
Adam Blake <theadamattack@gmail.com>
2019-01-08 20:26:12 -0800

    Updates documentation and consolidates duplicates

--- R/data.R
+++ R/data.R
@@ -1,56 +1,50 @@
-#' Exhibit 1.1
+#' US internet and college graduation rates
 #'
-#' Internet access rates for each of the 50 U.S. states.
+#' Internet access and college graduation rates for each of the 50 U.S. states
+#' in the year 2000.
 #'
-#' TODO: what are the units of the rates?
-#'
-#' @format A data frame with 50 observations of 3 variables:
+#' @format A data frame with 50 observations of 3 variables.
 #' \describe{
 #'   \item{State}{The two-letter abbreviation of the state.}
-#'   \item{Internet}{}
-#'   \item{College}{}
+#'   \item{Internet}{The percentage of households that had internet access.}
+#'   \item{College}{The percentage of residents with a college degree.}
 #' }
+#' @source <http://www.dataanalysisbook.com/datasetsInTextbook.html>
+#' @name US_internet_grad
+"US_internet_grad"
+
+#' @rdname US_internet_grad
 "ex1.1"
 
-#' Exhibit 4.1
-#'
-#' Lottery bids for 20 bidders in hypothetical lottery.
-#'
-#' @format A data frame with 20 observations of 1 variable:
-#' \describe{
-#'   \item{bids}{Bid in dollars.}
-#' }
-"ex4.1"
+#' @rdname US_internet_grad
+"ex2.9"
 
-#' Lottery dataset
+#' @rdname US_internet_grad
+"ex5.1"
+
+
+#' Hypothetical lottery bids
 #'
 #' Lottery bids for 20 bidders in hypothetical lottery.
 #'
-#' @format A data frame with 20 observations of 1 variable:
+#' @format A data frame with 20 observations of 1 variable.
 #' \describe{
 #'   \item{bids}{Bid in dollars.}
 #' }
+#' @source <http://www.dataanalysisbook.com/datasetsInTextbook.html>
+#' @name lottery
 "lottery"
 
-#' Exhibit 5.1
-#'
-#' Internet access rates and college education rates for each of the 50 U.S. states.
-#'
-#' TODO: what are the units of the rates?
-#'
-#' @format A data frame with 50 observations of 3 variables:
-#' \describe{
-#'   \item{State}{}
-#'   \item{Internet}{}
-#'   \item{College}{}
-#' }
-"ex5.1"
+#' @rdname lottery
+"ex4.1"
+
 
 #' Exhibit 6.2
 #'
-#' Sex, age (years), height (in.), and weight (lbs.) of 19 middle school students.
+#' Sex, age, height, and weight of 19 middle school students. This dataset
+#' appears in many of the examples in SAS software.
 #'
-#' @format A data frame with 19 observations of 5 variables:
+#' @format A data frame with 19 observations of 5 variables.
 #' \describe{
 #'   \item{Name}{Student's name}
 #'   \item{Sex}{Sex of the student: coded as F or M}
@@ -58,191 +52,226 @@
 #'   \item{Height}{Height of the student in inches}
 #'   \item{Weight}{Weight of the student in pounds}
 #' }
+#' @source <http://www.dataanalysisbook.com/datasetsInTextbook.html>
 "ex6.2"
 
-#' Exhibit 7.2
+#' Exhibit 7.1
 #'
 #' Running time, age, and miles of training per week for hypothetical runners.
 #'
-#' @format A data frame with 80 observations of 3 variables:
+#' @format A data frame with 80 observations of 3 variables.
 #' \describe{
 #'   \item{TIME}{Amount of time spent running per week}
 #'   \item{AGE}{Age of the runner}
 #'   \item{MILES}{Number of miles run per week}
 #' }
-"ex7.2"
+#' @source <http://www.dataanalysisbook.com/datasetsInTextbook.html>
+"ex7.1"
 
 #' Exhibit 8.1
 #'
 #' SAT scores for two groups of student who did or did not take a prep course.
 #'
-#' @format A data frame with 20 observations of 3 variables:
+#' @format A data frame with 20 observations of 3 variables.
 #' \describe{
 #'   \item{Student}{Student ID}
 #'   \item{Group}{Whether the students took the \code{course} or not \code{noCourse}}
 #'   \item{SAT}{Score on the SAT}
 #' }
+#' @source <http://www.dataanalysisbook.com/datasetsInTextbook.html>
 "ex8.1"
 
 #' Exhibit 8.6
 #'
-#' Subsequent tasks completed after different types of feedback on a previous task.
+#' Hypothetical experimental data for a motivation study where children perform
+#' a task, are given feedback, and then monitored for the number of tasks they
+#' subsequently complete. In one condition children were given feedback that
+#' they failed the task, other children were given no feedback, and in the last
+#' condition children were told they were successful.
 #'
-#' @format A data frame with 24 observations of 2 variables:
+#' @format A data frame with 24 observations of 2 variables.
 #' \describe{
 #'   \item{Condition}{Feeback given: failure, none, success}
-#'   \item{Task}{Score on the task}
+#'   \item{Task}{Number of tasks completed}
 #' }
+#' @source <http://www.dataanalysisbook.com/datasetsInTextbook.html>
 "ex8.6"
 
 #' Exhibit 8.13
 #'
-#' GRE scores from students in one of three training programs in psychology.
+#' Hypothetical GRE scores from students in one of four training programs in
+#' psychology.
 #'
-#' @format A data frame with 18 observations of 2 variables:
+#' @format A data frame with 18 observations of 2 variables.
 #' \describe{
-#'   \item{Program}{The program: clinical, developmental, or social}
+#'   \item{Program}{The program: clinical, developmental, experimental, or social}
 #'   \item{GRE}{Student's GRE score}
 #' }
+#' @source <http://www.dataanalysisbook.com/datasetsInTextbook.html>
 "ex8.13"
 
 #' Exhibit 8.15
 #'
-#' Scores on standardized arithmetic test for hypothetical schoolchildren.
+#' Hypothetical scores on a standardized arithmetic test for children in fourth,
+#' fifth, and sixth grades.
 #'
-#' @format A data frame with 15 observations of 2 variables:
+#' @format A data frame with 15 observations of 2 variables.
 #' \describe{
 #'   \item{Grade}{Student's current grade in school: 4, 5, or 6}
 #'   \item{Score}{Student's score on the test}
 #' }
+#' @source <http://www.dataanalysisbook.com/datasetsInTextbook.html>
 "ex8.15"
 
 #' Exhibit 9.1
 #'
-#' Mood scores from a drug x psychotherapy (treatment, control) factorial design.
+#' Hypothetical mood scores in a Drug (3) x Psychotherapy (2) factorial design.
 #'
-#' @format A data frame with 50 observations of 3 variables:
+#' @format A data frame with 50 observations of 3 variables.
 #' \describe{
-#'   \item{Drug}{The drug given: A, B, or Placebo}
-#'   \item{Psychotherapy}{Type of psychotherapy administered: treatment or control}
+#'   \item{Drug}{The drug given: DrugA, DrugB, or Placebo}
+#'   \item{Psychotherapy}{Psychotherapy condition: Treatment or Control}
 #'   \item{Mood}{Patient's mood score}
 #' }
+#' @source <http://www.dataanalysisbook.com/datasetsInTextbook.html>
 "ex9.1"
 
 #' Exhibit 10.1
 #'
-#' Acheivement and pretest scores for students under different curricula and teachers (effect coded groups).
+#' Acheivement and pretest scores for students under different curricula and
+#' teachers (effect coded groups).
 #'
-#' @format A data frame with 40 observations of 4 variables:
+#' @format A data frame with 40 observations of 4 variables.
 #' \describe{
 #'   \item{score}{The student's achievement score}
 #'   \item{curriculum}{-1 = old, 1 = new}
-#'   \item{teacher}{-1 = A, 1 = B}
-#'   \item{pretest}{The student's score on a pretest}
+#'   \item{teacher}{-1 = Teacher A, 1 = Teacher B}
+#'   \item{pretest}{The student's pretest achievement score}
 #' }
+#' @source <http://www.dataanalysisbook.com/datasetsInTextbook.html>
 "ex10.1"
 
 #' Exhibit 10.8
 #'
-#' Modified version of ex10.1.txt; see textbook for explanation.
+#' Acheivement and pretest scores for students under different curricula and
+#' teachers (effect coded groups). Similar to \code{\link{ex10.1}} except the
+#' pretest values are modified to have differences across the predictors
+#' \code{curriculum} and \code{teacher}.
 #'
-#' TODO: update explanation
-#'
-#' @format A data frame with 40 observations of 4 variables:
+#' @format A data frame with 40 observations of 4 variables.
 #' \describe{
 #'   \item{score}{The student's achievement score}
 #'   \item{curriculum}{-1 = old, 1 = new}
-#'   \item{teacher}{-1 = A, 1 = B}
-#'   \item{pretest}{The student's score on a pretest}
+#'   \item{teacher}{-1 = Teacher A, 1 = Teacher B}
+#'   \item{pretest}{The student's pretest achievement score}
 #' }
+#' @source <http://www.dataanalysisbook.com/datasetsInTextbook.html>
 "ex10.8"
 
 #' Exhibit 10.12
 #'
-#' Psychological symptoms and number of criticisms across two conditions.
+#' Hypothetical data for a family counseling intervention targetted at reducing
+#' bipolar disorder symptoms in adolescents. Families either received the
+#' experimental intervention treatment or usual care. The number of symptoms the
+#' child show were recorded, as well as the rate and degree of criticism from
+#' parents towards the child on a five point scale (higher scores mean more
+#' criticism).
 #'
-#' @format A data frame with 6 observations of 5 variables:
+#' @format A data frame with 6 observations of 5 variables.
 #' \describe{
-#'   \item{Condition}{Whether the participant was in the treatment or control group}
-#'   \item{Criticism}{Number of criticisms}
+#'   \item{Condition}{Whether the family was in the treatment or control group}
+#'   \item{Criticism}{Number of criticisms (1 to 5, where 5 is large)}
 #'   \item{Symptoms}{Number of symptoms}
 #' }
+#' @source <http://www.dataanalysisbook.com/datasetsInTextbook.html>
 "ex10.12"
 
 #' Exhibit 11.1
 #'
-#' Three within-group scores for two levels of competition.
+#' Hypothetical data for a nested design. Groups of 3 participants were asked to
+#' complete a task in the lab, and they were either told they were competing
+#' with other groups or not. After completing the task they rated their
+#' satisfaction with the group's work on a Likert scale (1 - 9, where 9 is most
+#' satisfied).
 #'
-#' @format A data frame with 6 observations of 5 variables:
+#' @format A data frame with 6 observations of 5 variables.
 #' \describe{
-#'   \item{group}{Group ID}
-#'   \item{instructions}{Whether the participants were instructed they were in \code{competition} or not (\code{none})}
-#'   \item{score1}{Score on first trial}
-#'   \item{score2}{Score on second trial}
-#'   \item{score3}{Score on third trial}
+#'   \item{group}{The group ID}
+#'   \item{instructions}{Whether the group was in \code{competition} or not (\code{none})}
+#'   \item{score1}{The first participant's satisfaction score (1-9, 9 is high)}
+#'   \item{score2}{The second participant's satisfaction score (1-9, 9 is high)}
+#'   \item{score3}{The third participant's satisfaction score (1-9, 9 is high)}
 #' }
+#' @source <http://www.dataanalysisbook.com/datasetsInTextbook.html>
 "ex11.1"
 
 #' Exhibit 11.9
 #'
-#' Data for crossed design (experimenter present or absent)
+#' Data from a crossed design where participants completed a set of math puzzles
+#' with the experimenter in the room and a set when the experimenter was absent.
 #'
-#' TODO: What do the scores represent?
-#'
-#' @format A data frame with 8 observations of 3 variables:
+#' @format A data frame with 8 observations of 3 variables.
 #' \describe{
 #'   \item{subject}{Subject ID}
-#'   \item{absent}{}
-#'   \item{present}{}
+#'   \item{absent}{Performance with experimenter absent (1 - 9, 9 is high)}
+#'   \item{present}{Performance with experimenter present (1 - 9, 9 is high)}
 #' }
+#' @source <http://www.dataanalysisbook.com/datasetsInTextbook.html>
 "ex11.9"
 
 #' Exhibit 11.17
 #'
-#' Data for a 3 x 2 within-subject design
+#' Data from a study where study time and word abstraction were manipulated to
+#' observe their effects on memory. Participants studied lists of either
+#' abstract or concrete words for either 1, 2, or 3 minutes. Participants
+#' took a test following each of the 6 lists.
 #'
-#' TODO: what was the experiment?
-#'
-#' @format A data frame with 5 observations of 7 variables:
+#' @format A data frame with 5 observations of 7 variables.
 #' \describe{
-#'   \item{Subject}{}
-#'   \item{abstract1}{}
-#'   \item{concrete1}{}
-#'   \item{abstract2}{}
-#'   \item{concrete2}{}
-#'   \item{abstract3}{}
-#'   \item{concrete3}{}
+#'   \item{Subject}{The participant's ID}
+#'   \item{abstract1}{Number of words recalled (abstract words, 1 minute)}
+#'   \item{concrete1}{Number of words recalled (concrete words, 1 minute)}
+#'   \item{abstract2}{Number of words recalled (abstract words, 2 minutes)}
+#'   \item{concrete2}{Number of words recalled (concrete words, 2 minutes)}
+#'   \item{abstract3}{Number of words recalled (abstract words, 3 minutes)}
+#'   \item{concrete3}{Number of words recalled (concrete words, 3 minutes)}
 #' }
+#' @source <http://www.dataanalysisbook.com/datasetsInTextbook.html>
 "ex11.17"
 
 #' Exhibit 11.22
 #'
-#' Scores for husbands and wives by couple with years married and child status.
+#' Ratings of marital satisfaction (1 - 9, where 9 is the greatest
+#' satisfaction) for 16 couples. Half of the couples were married for 15 years
+#' and half for 30 years. In the 15-year marriages, the couples had either 2 or
+#' more young children living at home or not, and in the 30-year marriages the
+#' couples either had 2 or more children grown and living away from home or not.
 #'
-#' TODO: What are the couple, Male and Female scores?
-#'
-#' @format A data frame with 16 observations of 5 variables:
+#' @format A data frame with 16 observations of 5 variables.
 #' \describe{
 #'   \item{couple}{}
-#'   \item{children}{Does the couple have children? ys or no}
+#'   \item{children}{Does the couple have children? yes or no}
 #'   \item{yearsmarried}{Number of years married}
-#'   \item{Male}{}
-#'   \item{Female}{}
+#'   \item{Male}{The husband's martial satisfaction (1 - 9, 9 is high)}
+#'   \item{Female}{The wife's martial satisfaction (1 - 9, 9 is high)}
 #' }
+#' @source <http://www.dataanalysisbook.com/datasetsInTextbook.html>
 "ex11.22"
 
 #' Exhibit 13.1
 #'
-#' SAT and high school ranks with and without an outlier induced by reversing variable values for one observations.
+#' SAT verbal (\code{sat}) and and high school ranks (\code{hsrank}) for 13
+#' students. These columns are repeated in \code{satr} and \code{hsrankr} with
+#' the exception of student 6, where the SAT and rank have been reversed in the
+#' \code{satr} and \code{hsrankr} varibles to create an outlier.
 #'
-#' TODO: these don't appear to be reversed. Check textbook for description and method
-#'
-#' @format A data frame with 13 observations of 5 variables:
+#' @format A data frame with 13 observations of 5 variables.
 #' \describe{
 #'   \item{student}{Student ID}
 #'   \item{sat}{Student's SAT score}
 #'   \item{hsrank}{Student's rank in high school}
-#'   \item{satr}{Reversed values of \code{sat}}
-#'   \item{hsrankr}{Reversed values of \code{hsrankr}}
+#'   \item{satr}{\code{sat} with student 6's score being their \code{hsrank}}
+#'   \item{hsrankr}{\code{hsrank} with student 6's score being their \code{sat}}
 #' }
+#' @source <http://www.dataanalysisbook.com/datasetsInTextbook.html>
 "ex13.1"
--- data-raw/process_data.R
+++ data-raw/process_data.R
@@ -1,15 +1,38 @@
+library(readr)
+
 # Process the data for this package
 #
 # The text files provided by the authors are already very clean and tidy, so
 # there is no true processing of the data in this file. The data are just read
 # in using read.csv and then passed to usethis::use_data to add to the package.
 
-file_paths <- dir("./data-raw", ".txt$", full.names = TRUE)
-data_names <- sub("\\.txt", "", basename(file_paths))
-contents <- lapply(file_paths, read.csv, encoding = "UTF-8", sep = "",
-                   stringsAsFactors = FALSE)
+get_data <- function(name, col_types) {
+  read_table2(file.path("./data-raw", name), col_types = col_types)
+}
 
-invisible(mapply(function(name, data) {
-  assign(name, data)
+US_internet_grad <- get_data("ex1.1.txt", "cdd")
+US_internet_grad -> ex1.1 -> ex2.9 -> ex5.1
+
+lottery <- get_data("lottery.txt", "d")
+lottery -> ex4.1
+
+ex6.2 <- get_data("ex6.2.txt", "ccidd")
+ex7.1 <- get_data("ex7.1.txt", "dii")
+ex8.1 <- get_data("ex8.1.txt", "ici")
+ex8.6 <- get_data("ex8.6.txt", "ci")
+ex8.13 <- get_data("ex8.13.txt", "ci")
+ex8.15 <- get_data("ex8.15.txt", "ii")
+ex9.1 <- get_data("ex9.1.txt", "cci")
+ex10.1 <- get_data("ex10.1.txt", "iiii")
+ex10.8 <- get_data("ex10.8.txt", "iiii")
+ex10.12 <- get_data("ex10.12.txt", "cii")
+ex11.1 <- get_data("ex11.1.txt", "iciii")
+ex11.9 <- get_data("ex11.9.txt", "iii")
+ex11.17 <- get_data("ex11.17.txt", "iiiiiii")
+ex11.22 <- get_data("ex11.22.txt", "iciii")
+ex13.1 <- get_data("ex13.1.txt", "iiiii")
+
+rm(get_data)
+invisible(lapply(ls(), function(name) {
   do.call(usethis::use_data, list(as.name(name), overwrite = TRUE))
-}, data_names, contents))
+}))
